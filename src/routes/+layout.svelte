69cfe061a29e3f73739216869be0249f44150199
Egor <egorurtaev9824@gmail.com>
2023-06-15 14:07:44 +0300

    first layout and styles added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "../styles/css/main.css"
+</script>
+
+<header>
+    <h1 id="main-h1">Сайт о Казани</h1>
+    <nav class="header-navbar">
+        <a href="#" class="header-nav-link">история</a>
+        <a href="#" class="header-nav-link">достопримечательности</a>
+        <a href="#" class="header-nav-link">учебные заведения</a>
+        <a href="#" class="header-nav-link">отдых</a>
+    </nav>
+</header>
+
+<slot />
+
+<footer>
+    <p>Егор Юртаев 2023</p>
+</footer>
