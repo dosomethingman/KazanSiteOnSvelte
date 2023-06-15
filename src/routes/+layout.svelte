42606e24a410119a5183194463125296584c548c
Egor <egorurtaev9824@gmail.com>
2023-06-15 15:45:46 +0300

    first pages prototype and styles added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,24 @@
 <script>
     import "../styles/css/main.css"
+    export let data;
 </script>
 
-<header>
+<header class="main-header">
     <h1 id="main-h1">Сайт о Казани</h1>
     <nav class="header-navbar">
-        <a href="#" class="header-nav-link">история</a>
+        <!--<a href="#" class="header-nav-link">история</a>
         <a href="#" class="header-nav-link">достопримечательности</a>
         <a href="#" class="header-nav-link">учебные заведения</a>
-        <a href="#" class="header-nav-link">отдых</a>
+        <a href="#" class="header-nav-link">отдых</a>-->
+        {#each data.summaries as { slug, title }}
+            <a href="./{ slug }" class="header-nav-link">{ title }</a>
+        {/each}
     </nav>
 </header>
 
-<slot />
+<main>
+    <slot />
+</main>
 
 <footer>
     <p>Егор Юртаев 2023</p>
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let data;
+</script>
+
+<article class="main-article">
+    <h1>{ data.post.title }</h1>
+    <p>{ data.post.text }</p>
+</article>
