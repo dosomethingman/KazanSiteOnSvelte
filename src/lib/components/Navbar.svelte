d1dff79463a50ceb87bb4e241474f0240cdd64ae
Egor <egorurtaev9824@gmail.com>
2023-06-20 21:00:39 +0300

    added low screen width navbar, svelte-media-queries installed

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,12 @@
 <script>
     export let navbarClass;
     export let linkClass;
+    export let homepageLinkClass;
     export let data;
 </script>
 
 <nav class="{ navbarClass }">
-    <a href="/" class="homepage-link">&#127968;</a>
+    <a href="/" class="{ homepageLinkClass }">&#127968;</a>
     <!--<a href="#" class="header-nav-link">история</a>
     <a href="#" class="header-nav-link">достопримечательности</a>
     <a href="#" class="header-nav-link">учебные заведения</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import "../styles/css/main.css"
     import Navbar from "$lib/components/Navbar.svelte";
+    import MediaQuery from 'svelte-media-queries'
+
     export let data;
 </script>
 
@@ -9,9 +11,19 @@
 </svelte:head>
 
 <header class="main-header">
-    <h1 id="main-h1">Сайт о Казани</h1>
-    <Navbar navbarClass="header-navbar" linkClass="header-nav-link" {data} />
+    <h1 id="main-h1">Сайт о Казани</h1>\
+    <MediaQuery query="(max-width: 520px)" let:matches>
+        {#if !matches}
+            <Navbar navbarClass="header-navbar" linkClass="header-nav-link" homepageLinkClass="homepage-link" {data} />
+        {/if}
+    </MediaQuery>
 </header>
+
+<MediaQuery query="(max-width: 520px)" let:matches>
+    {#if matches}
+        <Navbar navbarClass="navbar-low-res" linkClass="nav-link-low-res" homepageLinkClass="homepage-link-low-res" {data} />
+    {/if}
+</MediaQuery>
 
 <main>
     <slot />
